7cfa320f758617ac9e80745ecd64d34597fa6299
曹剑平 <caojianping@agilines.com>
2023-08-16 15:41:21 +0800

    feat:处理prettier配置

--- test-main/src/bootstrap.ts
+++ test-main/src/bootstrap.ts
@@ -4,13 +4,13 @@
  * @Date: 2023-08-03 17:24:34
  */
 
-import { createApp, defineAsyncComponent } from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import { createApp, defineAsyncComponent } from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
 
-import { mockFetch } from "./utils";
-import { RemoteLoader } from "./remote";
+import { mockFetch } from './utils';
+import { RemoteLoader } from './remote';
 
 async function render() {
   const app = createApp(App);
@@ -20,25 +20,25 @@ async function render() {
 
   // 第一种：异步组件注册测试
   app.component(
-    "HelloWorld",
-    defineAsyncComponent(() => import("@/components/HelloWorld.vue"))
+    'HelloWorld',
+    defineAsyncComponent(() => import('@/components/HelloWorld.vue'))
   );
 
   // 第二种：远程异步组件注册测试
   const remoteConfigs: any = await mockFetch([
     {
-      url: "http://localhost:9999/remoteEntry.js",
-      scope: "base",
-      module: "./BaseOne.vue",
+      url: 'http://localhost:9999/remoteEntry.js',
+      scope: 'base',
+      module: './BaseOne.vue',
     },
   ]);
   for (let i = 0; i < remoteConfigs.length; i++) {
-    loader.registerModule(remoteConfigs[i]);
+    await loader.registerModule(remoteConfigs[i]);
   }
 
   // 第三种：动态异步组件注册测试
-  await loader.registerModulesByOptions(["system/remotes"]);
-  app.mount("#app");
+  await loader.registerModulesByOptions(['system/remotes']);
+  app.mount('#app');
 }
 
 render();
--- test-system/.eslintrc.js
+++ test-system/.eslintrc.js
@@ -18,9 +18,14 @@ module.exports = {
     ecmaVersion: 2020,
   },
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "@typescript-eslint/no-explicit-any": "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-useless-escape': 'off',
+    'no-async-promise-executor': 'off',
+    '@typescript-eslint/no-unused-vars': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'prettier/prettier': 'off',
   },
   overrides: [
     {
